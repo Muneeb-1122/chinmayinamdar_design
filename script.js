58256af0371db64c268b1255ea6e4d5341b3d1ba
Muhammad Muneeb <muneebsaeed190@gamil.com>
2025-02-17 17:33:10 +0500

    changed the card css and apply the tailwind in cards

---
+++ script.js
@@ -0,0 +1,11 @@
+Shery.mouseFollower({
+  //Parameters are optional.
+  skew: true,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+});
+Shery.makeMagnet(".rotate" /* Element to target.*/, {
+  //Parameters are optional.
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+});
